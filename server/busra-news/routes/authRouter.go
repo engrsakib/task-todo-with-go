4365c015343b6e594a083206d8dbfd4a0dbfbcff
engrsakib <snazmussakib01@gmail.com>
2026-01-26 01:26:31 +0600

    update all factors

--- server/busra-news/routes/authRouter.go
+++ server/busra-news/routes/authRouter.go
@@ -2,24 +2,39 @@ package routes
 
 import (
 	"github.com/engrsakib/news-with-go/controllers"
-	"github.com/gin-gonic/gin"
 	"github.com/engrsakib/news-with-go/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(r *gin.Engine) {
 	
 	authGroup := r.Group("/api/auth")
+
+	
 	{
 		authGroup.POST("/register", controllers.RegisterUser)
-		authGroup.POST("/resend-otp", controllers.ResendOTP)
 		authGroup.POST("/login", controllers.LoginAsUser)
+		authGroup.POST("/refresh-token", controllers.RefreshToken)
 		authGroup.POST("/verify", controllers.VerifyOTP)
+		authGroup.POST("/resend-otp", controllers.ResendOTP)
 		authGroup.POST("/forgot-password", controllers.ForgotPassword)
 		authGroup.POST("/reset-password", controllers.ResetPassword)
-		authGroup.GET("/me", middleware.AuthMiddleware(), controllers.GetProfile)
-		authGroup.POST("/change-password", middleware.AuthMiddleware(), controllers.ChangePassword)
-		authGroup.PATCH("/update-profile", middleware.AuthMiddleware(), controllers.UpdateUser)
-		authGroup.PATCH("/admin/update-user", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), controllers.AdminUpdateUser)
-		authGroup.DELETE("/admin/delete-user/:id", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), controllers.DeleteUser)
+	}
+
+	protected := authGroup.Group("/")
+	protected.Use(middleware.AuthMiddleware())
+	{
+		protected.GET("/me", controllers.GetProfile)
+		protected.POST("/change-password", controllers.ChangePassword)
+		protected.PATCH("/update-profile", controllers.UpdateUser)
+
+	
+		admin := protected.Group("/admin")
+		admin.Use(middleware.RoleMiddleware("admin"))
+		{
+			admin.PATCH("/update-user", controllers.AdminUpdateUser)
+			
+			admin.DELETE("/delete-user/:id", controllers.DeleteUser)
+		}
 	}
 }
